8c30329e8a90b5dff475e4bb9f4f1b30bfff43fd
Liam Bowen <LiamBowen@gmail.com>
2021-12-29 15:36:53 -0500

    new routes for creation

--- todo-app/src/models.rs
+++ todo-app/src/models.rs
@@ -1,19 +1,39 @@
 use rocket::serde::{Serialize, Deserialize};
-use chrono::{DateTime, Utc};
-use crate::schema::tasks::dsl::*;
+use chrono::{NaiveDateTime};
 use diesel::prelude::*;
+use crate::schema::tasks;
 
 #[derive(Serialize, Deserialize, Queryable)]
 pub struct Task {
     pub id: i32,
     pub description: String,
     pub completed: bool,
-    pub created_at: chrono::NaiveDateTime,
+    pub created_at: NaiveDateTime,
 }
 
-pub fn load_tasks(conn: &diesel::PgConnection) -> Vec<Task> {
-    let results = tasks
-    .filter(completed.eq(false))
-    .load::<Task>(conn).expect("load tasks");
-    results
+#[derive(Serialize, Deserialize, Insertable)]
+#[table_name = "tasks"]
+pub struct NewTask {
+    pub description: String,
+    pub completed: bool,
+    pub created_at: NaiveDateTime,
 }
+
+impl Task {
+    pub fn all(conn: &diesel::PgConnection) -> Vec<Task> {
+        use crate::schema::tasks::dsl::*;
+        let results = tasks
+        .filter(completed.eq(false))
+        .load::<Task>(conn).expect("load tasks");
+        results
+    }
+    
+    pub fn create(conn: &diesel::PgConnection, t: NewTask) -> Task {
+        use crate::schema::tasks::dsl::*;
+        let result = diesel::insert_into(tasks)
+        .values(t)
+        .get_result(conn).expect("create task");
+        result
+    }
+}
+
--- todo-app/src/routes.rs
+++ todo-app/src/routes.rs
@@ -10,7 +10,14 @@ pub fn index() -> &'static str {
 #[get("/")]
 pub async fn tasks_index(conn: DbConn) -> Json<Vec<Task>> {
     let tasks: Vec<Task> = conn
-    .run(|c| load_tasks(c))
+    .run(|c| Task::all(c))
     .await;
     Json(tasks)
+}
+
+#[post("/", format="application/json", data = "<new_task>")]
+pub async fn tasks_create(conn: DbConn, new_task: Json<NewTask>) -> Json<Task> {
+    let t = new_task.0;
+    let task = conn.run(|c| Task::create(c, t)).await;
+    Json(task)
 }
