35a23925d262b7c0316ecbb72813df35e03084e8
Liam Bowen <LiamBowen@gmail.com>
2021-12-30 17:03:37 -0500

    add list and item components

---
+++ todo-web/src/TodoItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function TodoItem(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+TodoItem.propTypes = {
+
+}
+
+export default TodoItem
+
