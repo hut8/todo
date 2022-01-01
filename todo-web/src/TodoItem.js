85e8a03dd411d596a3418fe33ccb16e88e3a5c76
Liam Bowen <LiamBowen@gmail.com>
2022-01-01 18:19:12 -0500

    flesh out props

--- todo-web/src/TodoItem.js
+++ todo-web/src/TodoItem.js
@@ -2,15 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function TodoItem(props) {
+    let task = props.task;
     return (
-        <div>
-            
+        <div class="todo-item">
+            <div>{task.description}</div>
+            <div>{task.created_at}</div>
+            <div><input type="checkbox" checked={task.completed} /></div>
         </div>
     )
 }
 
 TodoItem.propTypes = {
-
+    description: PropTypes.string.isRequired,
+    created_at: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
 }
 
 export default TodoItem
--- todo-web/src/TodoList.js
+++ todo-web/src/TodoList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import TodoItem from './TodoItem'
 
 const TodoList = (props) => {
     return (
