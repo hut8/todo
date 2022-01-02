4ac719e493a87b0cf3847c93086070120cb78fdc
Liam Bowen <LiamBowen@gmail.com>
2022-01-02 17:00:02 -0500

    flesh out add todo a bit

--- todo-web/src/AddTodo.js
+++ todo-web/src/AddTodo.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 function AddTodo(props) {
+    const [description, setDescription] = useState("");
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        alert("Submitting");
+    }
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input
+                    value={description} 
+                    onChange={(e) => setDescription(e.target.value)}
+                    name="description" />
+                <input type="submit" />
+            </form>
         </div>
     )
 }
