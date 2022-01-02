0aca383f5f202fe3dfa40078ac2d07d139c3bd33
Liam Bowen <LiamBowen@gmail.com>
2022-01-01 19:03:53 -0500

    this is a hook not a prop

--- todo-web/src/TodoList.js
+++ todo-web/src/TodoList.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import TodoItem from './TodoItem'
+import { useState, useEffect } from 'react';
 
 const TodoList = (props) => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+    const LIST_URL = "https://127.0.0.1:8000/tasks";
+
+    useEffect(() => {
+        fetch(LIST_URL)
+          .then(res => res.json())
+          .then(
+            (result) => {
+              setIsLoaded(true);
+              setItems(result);
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              setIsLoaded(true);
+              setError(error);
+            }
+          )
+      }, []);
+
+    if (error) {
+        return (
+            <b>{error}</b>
+        );
+    }
+
     return (
         <>
-            {props.tasks.map((task) => (
+            {items.map((task) => (
                 <TodoItem task={task} />
             ))}
         </>
