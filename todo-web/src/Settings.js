cca1d706b5eed24d82ce9c677fd09be7e4b37091
Liam Bowen <LiamBowen@gmail.com>
2022-01-15 19:04:30 -0500

    factor out settings, start delete stuff

---
+++ todo-web/src/Settings.js
@@ -0,0 +1,5 @@
+const Settings = {
+    apiBase: new URL("http://127.0.0.1:8000/tasks"),
+};
+
+export default Settings;
--- todo-web/src/TodoItem.js
+++ todo-web/src/TodoItem.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Settings from './Settings';
+import axios from 'axios';
 
 function TodoItem(props) {
     let task = props.task;
 
     const completedChange = (e) => {
         console.log(`completedChange: ${e.target.value}`);
-    }
+    };
+
+    const deleteItem = (task) => {
+        let url = new URL(Settings.apiBase);
+        url.pathname = `${url.pathname}/${task.id}`;
+        console.log(`deleteItem: ${task.id}`);
+        let response = axios.delete(url);
+        response.then((v) => console.log("delete",task.id,v.statusText));
+    };
 
     return (
         <tr className="todo-item">
@@ -15,7 +25,9 @@ function TodoItem(props) {
             <td><input type="checkbox"
                 onChange={(e) => completedChange(e)}
                 checked={task.completed} /></td>
-            <td>❌</td>
+            <td style={{cursor: 'pointer'}} onClick={() => deleteItem(task)}>
+                ❌
+            </td>
         </tr>
     )
 }
